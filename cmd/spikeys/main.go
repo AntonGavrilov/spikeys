fa00903419645dd3dce0c45c46cb49bffb06f065
Anton Gavrilov <gavranton@gmail.com>
2024-11-28 22:02:15 +0300

    change git ignore

---
+++ cmd/spikeys/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/AntonGavrilov/spikeys/internal/benchmarking"
+	"github.com/AntonGavrilov/spikeys/internal/reporting"
+	"github.com/spf13/pflag"
+)
+
+func main() {
+	config := benchmarking.BenchmarkConfig{}
+	flagSet := pflag.NewFlagSet("benchmark", pflag.ContinueOnError)
+	err := benchmarking.LoadConfig(os.Args, flagSet, &config)
+
+	if err != nil {
+		if !errors.Is(err, pflag.ErrHelp) {
+			fmt.Println(err)
+			flagSet.PrintDefaults()
+		}
+		os.Exit(1)
+	}
+
+	result, err := benchmarking.RunBenchmark(config)
+
+	if err != nil {
+		if errors.Is(err, benchmarking.ErrBenchmarkTimeoutExcceded) ||
+			errors.Is(err, benchmarking.ErrRequestTimeoutExceeded) {
+			fmt.Println(err)
+		} else {
+			fmt.Println("Unexpecting error during benchmarking")
+		}
+		os.Exit(1)
+	}
+
+	summary := reporting.GenerateSummary(result.Results, result.Duration, int64(config.MaxConcurrency))
+	reporting.PrintSummary(summary)
+}
